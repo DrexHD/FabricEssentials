04618bcc62e73db2221029f19aa88474386b72d0
DrexHD <nicknamedrex@gmail.com>
2022-09-22 21:40:38 +0200

    add: KiloEssentials warp imports

--- src/main/java/org/server_utilities/essentials/command/impl/util/importer/KiloEssentialsImporter.java
+++ src/main/java/org/server_utilities/essentials/command/impl/util/importer/KiloEssentialsImporter.java
@@ -8,16 +8,15 @@ import net.minecraft.world.phys.Vec3;
 import org.server_utilities.essentials.EssentialsMod;
 import org.server_utilities.essentials.storage.DataStorage;
 import org.server_utilities.essentials.storage.PlayerData;
+import org.server_utilities.essentials.storage.ServerData;
 import org.server_utilities.essentials.util.teleportation.Home;
 import org.server_utilities.essentials.util.teleportation.Location;
+import org.server_utilities.essentials.util.teleportation.Warp;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import java.nio.file.Path;
+import java.util.*;
 
 public class KiloEssentialsImporter implements DataImporter {
 
@@ -34,13 +33,16 @@ public class KiloEssentialsImporter implements DataImporter {
     @Override
     public void importData(MinecraftServer server) {
         File workingDir = new File(System.getProperty("user.dir"));
-        File users = workingDir.toPath().resolve("essentials").resolve("data").resolve("users").toFile();
-        int failed = 0;
-        int success = 0;
+        Path data = workingDir.toPath().resolve("essentials").resolve("data");
+        File users = data.resolve("users").toFile();
         if (users.exists()) {
+            int failed = 0;
+            int success = 0;
             for (File file : Objects.requireNonNull(users.listFiles())) {
                 UUID uuid = UUID.fromString(file.getName().replace(".dat", ""));
+                boolean shouldSave = false;
                 try {
+                    PlayerData playerData = DataStorage.STORAGE.getOfflinePlayerData(server, uuid);
                     CompoundTag tag = NbtIo.readCompressed(new FileInputStream(file));
                     CompoundTag homesTag = tag.getCompound("homes");
                     List<Home> homes = new ArrayList<>();
@@ -51,19 +53,44 @@ public class KiloEssentialsImporter implements DataImporter {
                         homes.add(new Home(key, new Location(new Vec3(pos.getDouble("x"), pos.getDouble("y"), pos.getDouble("z")), view.getFloat("yaw"), view.getFloat("pitch"), new ResourceLocation(loc.getString("dim")))));
                     }
                     if (!homes.isEmpty()) {
-                        PlayerData playerData = DataStorage.STORAGE.getOfflinePlayerData(server, uuid);
+                        shouldSave = true;
                         playerData.getHomes().addAll(homes);
-                        DataStorage.STORAGE.saveOfflinePlayerData(server, uuid, playerData);
                     }
+                    CompoundTag settingsTag = tag.getCompound("settings");
+                    if (settingsTag.contains("rtps_left")) {
+                        shouldSave = true;
+                        playerData.rtpsLeft = settingsTag.getInt("rtps_left");
+                    }
+                    if (shouldSave) DataStorage.STORAGE.saveOfflinePlayerData(server, uuid, playerData);
                     success++;
-                } catch (IOException e) {
-                    EssentialsMod.LOGGER.error("An error occurred while reading user file", e);
+                } catch (Throwable e) {
+                    EssentialsMod.LOGGER.error("An error occurred while parsing user file", e);
                     failed++;
                 }
             }
-            EssentialsMod.LOGGER.info("Data imported, {} successful, {} failed!", success, failed);
+            EssentialsMod.LOGGER.info("User data imported, {} successful, {} failed!", success, failed);
         } else {
             EssentialsMod.LOGGER.error("User directory ({}) doesn't exist", users);
         }
+        File warpsFile = data.resolve("warps.dat").toFile();
+        if (warpsFile.exists()) {
+            try {
+                CompoundTag tag = NbtIo.readCompressed(new FileInputStream(warpsFile));
+                List<Warp> warps = new ArrayList<>();
+                for (String key : tag.getAllKeys()) {
+                    CompoundTag loc = tag.getCompound(key).getCompound("loc");
+                    CompoundTag pos = loc.getCompound("pos");
+                    CompoundTag view = loc.getCompound("view");
+                    warps.add(new Warp(key, new Location(new Vec3(pos.getDouble("x"), pos.getDouble("y"), pos.getDouble("z")), view.getFloat("yaw"), view.getFloat("pitch"), new ResourceLocation(loc.getString("dim")))));
+                }
+                ServerData serverData = DataStorage.STORAGE.getServerData();
+                serverData.getWarps().addAll(warps);
+                EssentialsMod.LOGGER.info("Warps data imported, imported {} warps!", warps.size());
+            } catch (Throwable e) {
+                EssentialsMod.LOGGER.error("An error occurred while parsing warps file", e);
+            }
+        }
+
+
     }
 }

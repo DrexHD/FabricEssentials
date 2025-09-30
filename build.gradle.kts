bf6bc5e4c41ce00435c58698ccf43eb41a79ae95
Drex <nicknamedrex@gmail.com>
2025-09-30 14:25:35 +0200

    Update stonecutter

---
+++ build.gradle.kts
@@ -0,0 +1,108 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.ChangelogPluginExtension
+
+plugins {
+    id("fabric-loom") version "1.11-SNAPSHOT"
+    id("maven-publish")
+    id("me.modmuss50.mod-publish-plugin") version "0.8.4"
+    id("org.jetbrains.changelog")
+}
+
+version = findProperty("mod_version") as String + "+" + findProperty("minecraft_version")
+group = findProperty("maven_group") as String
+
+base {
+    archivesName = findProperty("archives_base_name") as String
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(findProperty("java_version") as String)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.nucleoid.xyz/")
+    maven("https://api.modrinth.com/maven")
+}
+
+loom {
+    splitEnvironmentSourceSets()
+
+    runConfigs.all {
+        ideConfigGenerated(true)
+    }
+}
+
+fun DependencyHandlerScope.includeMod(dep: String) {
+    include(modImplementation(dep)!!)
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${findProperty("minecraft_version")}")
+    mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:${findProperty("loader_version")}")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${findProperty("fabric_version")}")
+
+    includeMod("me.lucko:fabric-permissions-api:${findProperty("permission_api_version")}")
+    includeMod("eu.pb4:sgui:${findProperty("sgui_version")}")
+    includeMod("eu.pb4:placeholder-api:${findProperty("placeholder_api_version")}")
+    includeMod("eu.pb4:player-data-api:${findProperty("player_data_api_version")}")
+    includeMod("eu.pb4:common-protection-api:${findProperty("common_protection_api_version")}")
+    includeMod("maven.modrinth:message-api:${findProperty("message_api_version")}")
+}
+
+publishMods {
+    file.set(tasks.remapJar.get().archiveFile)
+    type.set(STABLE)
+    changelog.set(fetchChangelog())
+
+    displayName = "Fabric Essentials ${version.get()}"
+    modLoaders.add("fabric")
+    modLoaders.add("quilt")
+
+
+    curseforge {
+        accessToken = providers.environmentVariable("CURSEFORGE_TOKEN")
+        projectId = "1271801"
+        minecraftVersions.addAll(findProperty("curseforge_minecraft_versions")!!.toString().split(", "))
+    }
+    modrinth {
+        accessToken = providers.environmentVariable("MODRINTH_TOKEN")
+        projectId = "o69N0FT2"
+        minecraftVersions.addAll(findProperty("modrinth_minecraft_versions")!!.toString().split(", "))
+    }
+    github {
+        accessToken = providers.environmentVariable("GITHUB_TOKEN")
+        repository = providers.environmentVariable("GITHUB_REPOSITORY").getOrElse("DrexHD/FabricEssentials")
+        commitish = providers.environmentVariable("GITHUB_REF_NAME").getOrElse("main")
+    }
+}
+
+tasks {
+    processResources {
+        val props = mapOf(
+            "version" to project.version,
+            "javaVersion" to findProperty("java_version")
+        )
+
+        inputs.properties(props)
+
+        filesMatching(listOf("fabric.mod.json", "*.mixins.json")) {
+            expand(props)
+        }
+    }
+}
+
+fun fetchChangelog(): String {
+    val log = rootProject.extensions.getByType<ChangelogPluginExtension>()
+    val modVersion = findProperty("mod_version")!!.toString()
+    return if (log.has(modVersion)) {
+        log.renderItem(
+            log.get(modVersion).withHeader(false),
+            Changelog.OutputType.MARKDOWN
+        )
+    } else {
+        ""
+    }
+}
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,17 +2,7 @@ plugins {
     id("dev.kikugie.stonecutter")
     id("org.jetbrains.changelog") version "2.2.0"
 }
-stonecutter active "1.21.6-rc1"
-
-stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) { 
-    group = "project"
-    ofTask("build")
-}
-
-stonecutter registerChiseled tasks.register("chiseledPublishMods", stonecutter.chiseled) {
-    group = "project"
-    ofTask("publishMods")
-}
+stonecutter active "1.21.8"
 
 changelog {
     path = rootProject.file("CHANGELOG.md").path

f15eb44d565c2ca5e34f974e1db3826989ce681e
Drex <nicknamedrex@gmail.com>
2026-06-16 17:40:26 +0200

    Use stonecutter comments for 26.2 update

--- src/main/java/me/drex/essentials/datagen/ModItemTagProvider.java
+++ src/main/java/me/drex/essentials/datagen/ModItemTagProvider.java
@@ -4,7 +4,11 @@ import me.drex.essentials.item.ModItemTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
+//? if >= 26.2 {
 import net.minecraft.references.BlockItemIds;
+//? } else {
+//import net.minecraft.world.item.Items;
+//? }
 
 import java.util.concurrent.CompletableFuture;
 
@@ -15,7 +19,12 @@ public class ModItemTagProvider extends FabricTagsProvider.ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider wrapperLookup) {
+        //? if >= 26.2 {
         this.builder(ModItemTags.HAT_DENY)
             .add(BlockItemIds.BEDROCK);
+        //? } else {
+        /*this.valueLookupBuilder(ModItemTags.HAT_DENY)
+            .add(Items.BEDROCK);*/
+        //? }
     }
 }
